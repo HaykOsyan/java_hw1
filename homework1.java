b28291557c8fcab737b0c8e3f501c66a10d99ff9
«HaykOsyan <ohayk@mail.ru>
2024-09-25 12:03:10 +0400

    Third task

--- homework1.java
+++ homework1.java
@@ -28,17 +28,36 @@ package homework1;
 // Напишите метод printEvenNums, который выведет на экран все четные числа
 // в промежутке от 1 до 100, каждое на новой строке.
 
+// public class homework1 {
+
+// public static void main(String[] args) {
+// printEvenNums(100);
+// }
+
+// public static void printEvenNums(int n) {
+// for (int i = 0; i < n; i++) {
+// if (i % 2 == 0) {
+// System.err.println(i);
+// }
+// }
+// }
+// }
+
+// Задача 3. Подсчет суммы цифр числа
+// Напишите метод sumDigits, который принимает целое число n и возвращает
+// сумму его цифр.
+
 public class homework1 {
-
     public static void main(String[] args) {
-        printEvenNums(100);
+        System.err.println(sumDigits(156));;
     }
-
-    public static void printEvenNums(int n) {
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                System.err.println(i);
-            }
+    public static int sumDigits(int n) {
+        int sum = 0;
+        String num = Integer.toString(n);
+        char[] charArray = num.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+            sum += Character.getNumericValue(charArray[i]);
         }
+        return sum;
     }
 }
