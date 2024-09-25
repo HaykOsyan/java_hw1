93b0ab5df57fd3ad5069260dacd7beb417fe357e
«HaykOsyan <ohayk@mail.ru>
2024-09-25 11:40:26 +0400

    Second task

--- homework1.java
+++ homework1.java
@@ -7,19 +7,38 @@ package homework1;
 /**
  * homework1
  */
+// public class homework1 {
+
+// public static void main(String[] args) {
+// System.err.println(factorial(5));
+// }
+
+// public static int factorial(int n) {
+// if (n < 0) {
+// return -1;
+// } else if (n == 0) {
+// return 1;
+// } else {
+// return n * factorial(n - 1);
+// }
+// }
+// }
+
+// Задача 2. Вывод всех четных чисел от 1 до 100
+// Напишите метод printEvenNums, который выведет на экран все четные числа
+// в промежутке от 1 до 100, каждое на новой строке.
+
 public class homework1 {
 
     public static void main(String[] args) {
-        System.err.println(factorial(5));
+        printEvenNums(100);
     }
 
-    public static int factorial(int n) {
-        if (n < 0) {
-            return -1;
-        } else if (n == 0) {
-            return 1;
-        } else {
-            return n * factorial(n - 1);
+    public static void printEvenNums(int n) {
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                System.err.println(i);
+            }
         }
     }
 }
